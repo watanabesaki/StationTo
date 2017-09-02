6290cba7e63427159229fc892c97e31ccc5a6b23
渡辺早紀 <watanabesaki@watanabesakinoMacBook.local>
2017-09-03 00:08:26 +0900

    プレイスピッカーをcocoapodで入れ、指定した緯度経度近くのプレイスを検索、選択して表示

--- Station To/CheckinViewController.swift
+++ Station To/CheckinViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 import GooglePlaces
 
+import GooglePlacePicker
 
 class CheckinViewController: UIViewController {
     
@@ -20,6 +21,9 @@ class CheckinViewController: UIViewController {
     
     //現在地の住所
     @IBOutlet var addressLabel : UILabel!
+    
+    @IBOutlet var herenameLabel : UILabel!
+    @IBOutlet var hereaddressLabel : UILabel!
 
 
     override func viewDidLoad() {
@@ -45,18 +49,48 @@ class CheckinViewController: UIViewController {
                 return
             }
             
-            self.nameLabel.text = "No current place"
-            self.addressLabel.text = ""
+            self.herenameLabel.text = "No current place"
+            self.hereaddressLabel.text = ""
             
             if let placeLikelihoodList = placeLikelihoodList {
                 let place = placeLikelihoodList.likelihoods.first?.place
                 if let place = place {
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ",")
+                    self.herenameLabel.text = place.name
+                    self.hereaddressLabel.text = place.formattedAddress?.components(separatedBy: ",")
                         .joined(separator: "\n")
                     print(place.name)
                     print(place.formattedAddress?.components(separatedBy: ",").joined(separator: "\n") ?? String())
                 }
+            }
+        })
+    }
+    
+    //Place Picker を追加する
+    @IBAction func pickPlace(_ sender: UIButton) {
+        
+        //現在地の緯度経度
+        let center = CLLocationCoordinate2D(latitude: 35.6709056, longitude: 139.7577372)
+        
+        let northEast = CLLocationCoordinate2D(latitude: center.latitude + 0.001, longitude: center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2D(latitude: center.latitude - 0.001, longitude: center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        let placePicker = GMSPlacePicker(config: config)
+        
+        placePicker.pickPlace(callback: {(place, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            //現在地候補から選ぶと名前、住所の表示
+            if let place = place {
+                self.nameLabel.text = place.name
+                self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                    .joined(separator: "\n")
+            } else {
+                self.nameLabel.text = "No place selected"
+                self.addressLabel.text = ""
             }
         })
     }
