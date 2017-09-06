dbfe3009ded67feb5250bc446884f8f3b28679f5
渡辺早紀 <watanabesaki@watanabesakinoMacBook.local>
2017-09-06 15:17:27 +0900

    ピッカー複数対応

--- Station To/CheckinViewController.swift
+++ Station To/CheckinViewController.swift
@@ -12,7 +12,7 @@ import GooglePlaces
 
 import GooglePlacePicker
 
-class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
     
     //駅名入力
     @IBOutlet var stationInput : UITextField!
@@ -32,40 +32,40 @@ class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     //何分
     @IBOutlet var timeInput : UITextField!
     let timeinputList = ["","3","5","10","15","20","25","30","40","50","60"]
-        
-        
-        
+    
+    
+    
     //ピッカービュー
     var pickerview1 : UIPickerView = UIPickerView()
     var pickerview2 : UIPickerView = UIPickerView()
     var pickerview3 : UIPickerView = UIPickerView()
     var pickerview4 : UIPickerView = UIPickerView()
-        
-        
-        
-        
+    
+    
+    
+    
     //googleplace現在地の取得
     var placesClient = GMSPlacesClient()
-        
+    
     //チェックインした場所の名前、住所
     @IBOutlet var nameLabel : UILabel!
     @IBOutlet var addressLabel : UILabel!
-        
+    
     //現在地の名前、住所
     @IBOutlet var herenameLabel : UILabel!
     @IBOutlet var hereaddressLabel : UILabel!
-        
-        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            
+        
         placesClient = GMSPlacesClient.shared()
-            
+        
         //この画面で有効にする
         pickerview1.delegate = self
         pickerview1.dataSource = self
         pickerview1.showsSelectionIndicator = true
-    
+        
         pickerview2.delegate = self
         pickerview2.dataSource = self
         pickerview2.showsSelectionIndicator = true
@@ -82,39 +82,50 @@ class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         let toolbar = UIToolbar(frame: CGRectMake(0, 0, 0, 35))
         let doneItem = UIBarButtonItem(title: "完了", style: .plain, target: self, action: #selector(CheckinViewController.done))
         
-        let canselItem = UIBarButtonItem(title: "キャンセル", style: .plain, target: self, action: #selector(CheckinViewController.cancel))
+        //let canselItem = UIBarButtonItem(title: "キャンセル", style: .plain, target: self, action: #selector(CheckinViewController.cancel))
         
-        toolbar.setItems([canselItem, doneItem], animated: true)
-            
+        toolbar.setItems([doneItem], animated: true)
+        
+        self.stationInput.tag = 1
+        self.stationInput.delegate = self
         self.stationInput.inputView = pickerview1
         self.stationInput.inputAccessoryView = toolbar
         pickerview1.tag = 1
         
+        self.lineInput.tag = 2
+        self.lineInput.delegate = self
         self.lineInput.inputView = pickerview2
         self.lineInput.inputAccessoryView = toolbar
         pickerview2.tag = 2
-
-
+        
+        self.numberOfLineInput.tag = 3
+        self.numberOfLineInput.delegate = self
         self.numberOfLineInput.inputView = pickerview3
         self.numberOfLineInput.inputAccessoryView = toolbar
         pickerview3.tag = 3
-
-
+        
+        self.timeInput.tag = 4
+        self.timeInput.delegate = self
         self.timeInput.inputView = pickerview4
         self.timeInput.inputAccessoryView = toolbar
         pickerview4.tag = 4
-
         
         
-
+        
+        
         
     }
-        
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-            
-    }
         
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(textField.tag)
+        return true
+    }
+    
     //PickerViewに表示する列数を返す
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -123,11 +134,13 @@ class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     //PickerViewに表示する行数を返す
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         
-        if pickerview1.tag == 1{
+        print(pickerView.tag)
+        
+        if pickerView.tag == 1 {
             return stationInputList.count
-        }else if pickerview2.tag == 2{
+        }else if pickerView.tag == 2 {
             return lineInputList.count
-        }else if pickerview3.tag == 3{
+        }else if pickerView.tag == 3 {
             return numberOfLineInputList.count
         }else{
             return timeinputList.count
@@ -137,125 +150,139 @@ class CheckinViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     
     //Picker Viewに表示する値を返す
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerview1.tag == 1{
+        if pickerView.tag == 1{
             return stationInputList[row]
-        }else if pickerview2.tag == 2{
+        }else if pickerView.tag == 2 {
             return lineInputList[row]
-        }else if pickerview3.tag == 3{
+        }else if pickerView.tag == 3 {
             return numberOfLineInputList[row]
         }else{
             return timeinputList[row]
         }
     }
-        
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerview1.tag == 1{
+        if pickerView.tag == 1{
             self.stationInput.text = stationInputList[row]
-        }else if pickerview2.tag == 2{
+        }else if pickerView.tag == 2{
             self.lineInput.text = lineInputList[row]
-        }else if pickerview2.tag == 2{
+        }else if pickerView.tag == 3{
             self.numberOfLineInput.text = numberOfLineInputList[row]
         }else{
             self.timeInput.text = timeinputList[row]
         }
     }
-        
+
+    /*
     func cancel() {
-        if pickerview1.tag == 1{
-            self.stationInput.text = ""
-            self.stationInput.endEditing(true)
-        }else if pickerview2.tag == 2{
-            self.lineInput.text = ""
-            self.lineInput.endEditing(true)
-
-        }else if pickerview3.tag == 3{
-            self.numberOfLineInput.text = ""
-            self.numberOfLineInput.endEditing(true)
-
-        }else{
-            self.timeInput.text = ""
-            self.timeInput.endEditing(true)
-        }
+        
+        self.stationInput.endEditing(true)
+        self.lineInput.endEditing(true)
+        self.numberOfLineInput.endEditing(true)
+        self.timeInput.endEditing(true)
+        
+//        if pickerview1.tag == 1{
+//            self.stationInput.text = ""
+//            self.stationInput.endEditing(true)
+//        }else if pickerview2.tag == 2{
+//            self.lineInput.text = ""
+//            self.lineInput.endEditing(true)
+//        }else if pickerview3.tag == 3{
+//            self.numberOfLineInput.text = ""
+//            self.numberOfLineInput.endEditing(true)
+//            
+//        }else{
+//            self.timeInput.text = ""
+//            self.timeInput.endEditing(true)
+//        }
         
     }
-        
+     */
+    
     func done() {
-        
+        self.stationInput.endEditing(true)
+        self.lineInput.endEditing(true)
+        self.numberOfLineInput.endEditing(true)
+        self.timeInput.endEditing(true)
+
+
+        /*
         if pickerview1.tag == 1 {
             self.stationInput.endEditing(true)
         }else if pickerview2.tag == 2{
             self.lineInput.endEditing(true)
         }else if pickerview3.tag == 3{
             self.numberOfLineInput.endEditing(true)
-
+            
         }else{
             self.timeInput.endEditing(true)
-
+            
         }
+         */
         
-
+        
     }
-        
+    
     func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
         return CGRect(x: x, y: y, width: width, height: height)
     }
+    
+    
+    //現在のプレイスを取得する 端末の現在地にあるローカル ビジネスなどのプレイスを検出するには、GMSPlacesClient currentPlaceWithCallback: を呼び出す
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
         
-        
-        //現在のプレイスを取得する 端末の現在地にあるローカル ビジネスなどのプレイスを検出するには、GMSPlacesClient currentPlaceWithCallback: を呼び出す
-        @IBAction func getCurrentPlace(_ sender: UIButton) {
+        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
             
-            placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
-                if let error = error {
-                    print("Pick Place error: \(error.localizedDescription)")
-                    return
-                }
-                
-                self.herenameLabel.text = "No current place"
-                self.hereaddressLabel.text = ""
-                
-                if let placeLikelihoodList = placeLikelihoodList {
-                    let place = placeLikelihoodList.likelihoods.first?.place
-                    if let place = place {
-                        self.herenameLabel.text = place.name
-                        self.hereaddressLabel.text = place.formattedAddress?.components(separatedBy: ",")
-                            .joined(separator: "\n")
-                        print(place.name)
-                        print(place.formattedAddress?.components(separatedBy: ",").joined(separator: "\n") ?? String())
-                    }
-                }
-            })
-        }
-        
-        //Place Picker を追加する
-        @IBAction func pickPlace(_ sender: UIButton) {
+            self.herenameLabel.text = "No current place"
+            self.hereaddressLabel.text = ""
             
-            //現在地の緯度経度
-            let center = CLLocationCoordinate2D(latitude: 35.6709056, longitude: 139.7577372)
-            
-            let northEast = CLLocationCoordinate2D(latitude: center.latitude + 0.001, longitude: center.longitude + 0.001)
-            let southWest = CLLocationCoordinate2D(latitude: center.latitude - 0.001, longitude: center.longitude - 0.001)
-            let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
-            let config = GMSPlacePickerConfig(viewport: viewport)
-            let placePicker = GMSPlacePicker(config: config)
-            
-            placePicker.pickPlace(callback: {(place, error) -> Void in
-                if let error = error {
-                    print("Pick Place error: \(error.localizedDescription)")
-                    return
-                }
-                
-                //現在地候補から選ぶと名前、住所の表示
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
                 if let place = place {
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                    self.herenameLabel.text = place.name
+                    self.hereaddressLabel.text = place.formattedAddress?.components(separatedBy: ",")
                         .joined(separator: "\n")
-                } else {
-                    self.nameLabel.text = "場所が選択されていません"
-                    self.addressLabel.text = ""
+                    print(place.name)
+                    print(place.formattedAddress?.components(separatedBy: ",").joined(separator: "\n") ?? String())
                 }
-            })
-        }
+            }
+        })
+    }
+    
+    //Place Picker を追加する
+    @IBAction func pickPlace(_ sender: UIButton) {
         
+        //現在地の緯度経度
+        let center = CLLocationCoordinate2D(latitude: 35.6709056, longitude: 139.7577372)
         
+        let northEast = CLLocationCoordinate2D(latitude: center.latitude + 0.001, longitude: center.longitude + 0.001)
+        let southWest = CLLocationCoordinate2D(latitude: center.latitude - 0.001, longitude: center.longitude - 0.001)
+        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
+        let config = GMSPlacePickerConfig(viewport: viewport)
+        let placePicker = GMSPlacePicker(config: config)
         
+        placePicker.pickPlace(callback: {(place, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            //現在地候補から選ぶと名前、住所の表示
+            if let place = place {
+                self.nameLabel.text = place.name
+                self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                    .joined(separator: "\n")
+            } else {
+                self.nameLabel.text = "場所が選択されていません"
+                self.addressLabel.text = ""
+            }
+        })
+    }
+    
+    
+    
 }
