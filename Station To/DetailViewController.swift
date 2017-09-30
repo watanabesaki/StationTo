2755e0b7706b3d177038800fbdc27bafffa5a883
渡辺早紀 <watanabesaki@watanabesakinoMacBook.local>
2017-09-30 23:58:52 +0900

    マイページのチャックイン履歴表示

--- Station To/DetailViewController.swift
+++ Station To/DetailViewController.swift
@@ -7,16 +7,34 @@
 //
 
 import UIKit
+import NCMB
+import SVProgressHUD
+
 
 class DetailViewController: UIViewController {
     
+    var places : [NCMBObject] = []
+    
+    @IBOutlet var detailnameLabel : UILabel!
+    var detatilname : String!
     
     @IBOutlet var detaildateLabel : UILabel!
-    @IBOutlet var detailadressLabel : UILabel!
+    var detatildate : String = ""
+    
     @IBOutlet var detailstationLabel : UILabel!
+    var detatilstation : String = ""
+    
+    @IBOutlet var detaillineLabel : UILabel!
+    var detatilline :String = ""
+    
     @IBOutlet var detailexitLabel : UILabel!
+    var detatilexit : String = ""
+    
     @IBOutlet var detailnumberoftrainLabel : UILabel!
+    var detatilnumberoftrain : String = ""
+    
     @IBOutlet var detailtimeLabel : UILabel!
+    var detatitime : String = ""
     
     @IBOutlet var detailmemoTextView : UITextView!
 
@@ -25,7 +43,17 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        showdetail()
+        
+        detailnameLabel.text = detatilname
+        detaildateLabel.text = detatildate
+        detailstationLabel.text = detatilstation
+        detailexitLabel.text = detatilexit
+        detaillineLabel.text = detatilline
+        detailnumberoftrainLabel.text = detatilnumberoftrain
+        detailtimeLabel.text = detatitime
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,8 +61,97 @@ class DetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        reloadInputViews()
+    }
     
     
+    //チェックイン履歴の表示NCMBデータの取得
+    func showdetail(){
+        let query = NCMBQuery(className: "Place")
+        query?.findObjectsInBackground({ (result, error) in
+            if error != nil {
+                print("チェックイン履歴詳細表示エラー")
+            } else {
+                self.places = result as! [NCMBObject]
+                print(self.places.count)
+                
+                if self.places != nil{
+                    print("チェックイン履歴はありません")
+                    
+                }else{
+                    for place in self.places {
+                        let name = place.object(forKey: "name") as! String
+                        let date = place.object(forKey: "createDate") as! String
+                        let station = place.object(forKey: "station") as! String
+                        let exit = place.object(forKey: "exit") as! String
+                        let line = place.object(forKey: "line") as! String
+                        let trainnumber = place.object(forKey: "trainnumber") as! String
+                        let time = place.object(forKey: "time") as! String
+                        
 
+                        self.detatildate = date
+                        self.detatilstation = station
+                        self.detatilexit = exit
+                        self.detatilline = line
+                        self.detatilnumberoftrain = trainnumber
+                        self.detatitime = time
+                        
+                    }
+
+                }
+                
+            }
+        })
+    }
+
+/*
+    //メモの更新
+    @IBAction func memoupdate() {
+        //NCMBに書き込み保存
+        let object = NCMBQuery(className: "Place")
+        //query?.whereKey("name",equalTo: "")
+        
+        object?.findObjectsInBackground({ (result,error) in
+            if error != nil {
+                SVProgressHUD.showError(withStatus: error?.localizedDescription)
+                print("保存する場所エラー")
+            } else {
+                let memo = result as! [NCMBObject]
+                let text = memo.first
+                text?.setObject("", forKey: "memo")
+                //self.detailnameLabel.text =
+                /*
+                object?.saveInBackground ({ (error) in
+                        if error != nil {
+                            //保存に失敗
+                            SVProgressHUD.showError(withStatus: error!.localizedDescription)
+                            print("保存失敗")
+                        } else {
+                            //保存に成功
+                            SVProgressHUD.showSuccess(withStatus: "更新完了")
+                            
+                            //指定した秒数後に処理を実行
+                            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+                                SVProgressHUD.dismiss()
+                            }
+                            
+                        }
+                    })
+                
+            }
+            }
+        )}*/
+
+                
+
+        
+    
+
+
+
+
+
+
+}*/
 }
--- Station To/MypageViewController.swift
+++ Station To/MypageViewController.swift
@@ -16,7 +16,7 @@ class MypageViewController: UIViewController,UITableViewDataSource,UITableViewDe
     
     var namemember : [String] = []
     var datemember : [String] = []
-    var places : [NCMBObject] = []
+    var places = [NCMBObject]()
     
     //TableViewの宣言
     @IBOutlet var historyTableView : UITableView!
@@ -48,6 +48,18 @@ class MypageViewController: UIViewController,UITableViewDataSource,UITableViewDe
         // Dispose of any resources that can be recreated.
     }
     
+    //画面遷移、次の画面へ選択した場所名を渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let id = segue.identifier {
+            if id == "toDetail" {
+                let indexPath = historyTableView.indexPathForSelectedRow!
+                let detailViewController = segue.destination as! DetailViewController
+                detailViewController.detatilname = namemember[indexPath.row]
+            }
+        }
+    }
+
+    
     
     //チェックイン履歴の表示NCMBデータの取得
     func showhistory(){
@@ -56,14 +68,36 @@ class MypageViewController: UIViewController,UITableViewDataSource,UITableViewDe
             if error != nil {
                 print("マイページ履歴error")
             } else {
-                self.places = result as! [NCMBObject]
-                print(self.places.count)
+                //履歴の読み込みが可能
+                let places = result as! [NCMBObject]
+                print(places)
                 
-                if self.places != nil{
+                if places != nil{
+                    
+                    for places in places {
+                        let name = places.object(forKey: "name") as! String
+                        let date = places.object(forKey: "createDate") as! String
+                        
+                        self.namemember.append(name)
+                        self.datemember.append(date)
+                        
+                    }
+                    print(self.namemember)
+                    
+                    self.historyTableView.reloadData()
+                }else{
+                    print("チェックイン履歴がありません")
+                    self.historyLabel.text = "チェックイン履歴がありません"
+
+                }
+
+                
+                //履歴があるかないか
+                /*if place != nil{
                     print("チェックイン履歴がありません")
                     self.historyLabel.text = "チェックイン履歴がありません"
                 }else{
-                    for place in self.places {
+                    for place in place {
                         let name = place.object(forKey: "name") as! String
                         let date = place.object(forKey: "createDate") as! String
                         
@@ -72,10 +106,9 @@ class MypageViewController: UIViewController,UITableViewDataSource,UITableViewDe
                         
                     }
                     print(self.namemember)
-                    print(self.datemember)
-                    
+                 
                     self.historyTableView.reloadData()
-                }
+                }*/
                 
             }
         })
@@ -83,7 +116,7 @@ class MypageViewController: UIViewController,UITableViewDataSource,UITableViewDe
 
     //tableviewに表示するデータの個数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.places.count
+        return namemember.count
     }
     
     //tableviewに表示するデータの内容
